bb402803ea9856d91e0234fc742117372ab08718
anchal-Singh12 <anchal923675@gmail.com>
2025-11-15 20:17:45 +0530

    Add files via upload

--- Super.java
+++ Super.java
@@ -1,22 +1,17 @@
-class A1{
-    void show(){
-        System.out.println("It's our A1 show function...");
-    }
-
+ class A {
+    int x=20;
+    
 }
-class A2 extends A1{
+class B extends A{
+    int x=29;
     void show(){
-        System.err.println("It's our A2 show function...");
-        super.show();
-       
+        System.out.println(x);
+        System.out.println(super.x);
     }
 }
 public class Super{
     public static void main(String[] args) {
-        A2 obj=new A2();
-        obj.show();
-       
-        
+        B r=new B();
+        r.show();
     }
-
 }
